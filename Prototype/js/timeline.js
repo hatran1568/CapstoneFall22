50fcbce1db8ade4e67ea5da24763f47d64c20228
DanielChendyrov <linhhonbaothu45@gmail.com>
2022-09-20 04:40:33 +0700

    Adding edit form (can't update change yet)

--- Prototype/js/timeline.js
+++ Prototype/js/timeline.js
@@ -1,7 +1,13 @@
-$(function(){
-    $(".btn-delete").click(function(){
-        if(confirm("Do you want to delete this item in your itinerary?")){
+$(function () {
+    $(".btn-delete").click(function () {
+        if (confirm("Do you want to delete this item in your itinerary?")) {
             $(this).closest(".timeline-item").css("display", "none");
         }
+    });
+    $(".btn-edit").click(function () {
+        $(".edit-form").css("display", "block");
+    });
+    $(".btn-off").click(function () {
+        $(".edit-form").css("display", "none");
     });
 });
