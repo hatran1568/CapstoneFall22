04379dad67acd5235342c1517b4a398e56528587
DanielChendyrov <linhhonbaothu45@gmail.com>
2022-09-20 15:08:48 +0700

    Change UI languages to EN and add edit function

--- Prototype/js/timeline.js
+++ Prototype/js/timeline.js
@@ -4,10 +4,20 @@ $(function () {
             $(this).closest(".timeline-item").css("display", "none");
         }
     });
+
+    var item;
     $(".btn-edit").click(function () {
+        item = $(this).closest(".timeline-item");
         $(".edit-form").css("display", "block");
     });
     $(".btn-off").click(function () {
+        $(".edit-form").css("display", "none");
+    });
+    $(".btn-save").click(function () {
+        item.find(".name-value").html($("#placeName").val());
+        item.find(".date-value").html($("#date").val());
+        item.find(".time-value").html($("#time").val());
+        item.find(".address-value").html($("#address").val());
         $(".edit-form").css("display", "none");
     });
 });
