3d404f3b5483604ca3f8fa613199bc8a76d4bd3d
DanielChendyrov <linhhonbaothu45@gmail.com>
2022-09-20 16:52:16 +0700

    a little update

--- Prototype/js/timeline.js
+++ Prototype/js/timeline.js
@@ -15,36 +15,42 @@ $(function () {
         $(".add-form").css("display", "none");
     });
     $(".btn-save").click(function () {
+        var startTime = $("#startTime").val();
+        let startDate = new Date($("date").val().toString() + "T" + $("startTime").val().toString());
+        let endDate = new Date(startDate.valueOf());
+        endDate.setTime(endDate.getTime() + $("#duration").val() * 60 * 60 * 1000);
+        var endTime = String(endDate.getHours()).padStart(2, "0") + ":" + String(endDate.getMinutes()).padStart(2, "0");
         item.find(".name-value").html($("#placeName").val());
         item.find(".date-value").html($("#date").val());
-        item.find(".time-value").html($("#time").val());
+        item.find(".time-value").html(startTime + " - " + endTime);
         item.find(".address-value").html($("#address").val());
         $(".edit-form").css("display", "none");
     });
 
-    $("#btnAdd").click(function(){
+    $("#btnAdd").click(function () {
         $(".add-form").css("display", "block");
     });
-    $("#addBtnSave").click(function(){
+    $("#addBtnSave").click(function () {
         var startTime = $("#timeAdd").val();
-        let dateStart = new Date($('#dateAdd').val().toString()+'T'+ $('#timeAdd').val().toString())
-        let dateEnd = new Date(dateStart.valueOf())
-        dateEnd.setTime(dateEnd.getTime() + ($('#durationAdd').val()*60*60*1000));
-        var endTime = String(dateEnd.getHours()).padStart(2, '0') + ":" + String(dateEnd.getMinutes()).padStart(2, '0');
+        let dateStart = new Date($("#dateAdd").val().toString() + "T" + $("#timeAdd").val().toString());
+        let dateEnd = new Date(dateStart.valueOf());
+        dateEnd.setTime(dateEnd.getTime() + $("#durationAdd").val() * 60 * 60 * 1000);
+        var endTime = String(dateEnd.getHours()).padStart(2, "0") + ":" + String(dateEnd.getMinutes()).padStart(2, "0");
         console.log(startTime);
-        $(".timeline").children(".timeline-item").each(function(){
-            var startTimeItem = $(this).find(".time-value").html().split(" - ")[0];
-            if (startTimeItem >= startTime){
-                var item = $(".timeline li:first").clone();
-                item.find(".name-value").html($("#placeNameAdd").val());
-                item.find(".date-value").html($("#dateAdd").val());
-                item.find(".address-value").html($("#addressAdd").val());
-                item.find("time-value").html(startTime + " - " + endTime);
-                $(this).after(item);
-                return false;
-            }
-            
-        });
+        $(".timeline")
+            .children(".timeline-item")
+            .each(function () {
+                var startTimeItem = $(this).find(".time-value").html().split(" - ")[0];
+                if (startTimeItem >= startTime) {
+                    var item = $(".timeline li:first").clone();
+                    item.find(".name-value").html($("#placeNameAdd").val());
+                    item.find(".date-value").html($("#dateAdd").val());
+                    item.find(".address-value").html($("#addressAdd").val());
+                    item.find("time-value").html(startTime + " - " + endTime);
+                    $(this).after(item);
+                    return false;
+                }
+            });
         $(".add-form").css("display", "none");
     });
 });
