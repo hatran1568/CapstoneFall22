6b50963d4cf86ca1c295354839c44e80dc3796d2
giangtthe153299 <giangtong09@gmail.com>
2022-09-28 20:47:17 +0700

    Initial Database

---
+++ backend/backendserver/src/main/java/entity/Request.java
@@ -0,0 +1,55 @@
+package entity;
+
+
+import lombok.Data;
+
+import javax.persistence.*;
+
+@Data
+@Entity
+@Table(name="request")
+public class Request {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name="request_id",columnDefinition = "INT(1)")
+    private int requestId;
+
+    @ManyToOne
+    @JoinColumn(name = "poi_id", nullable = false)
+    private POI poi;
+
+    @ManyToOne
+    @JoinColumn(name = "user_id", nullable = false)
+    private User user;
+
+    @Column(name="Name")
+    private String name;
+
+
+
+    @Column(name="address")
+    private String Address;
+
+
+
+    @Column(name="telephone_number")
+    private String phone;
+
+    @Column(name="business_email")
+    private String businessEmail;
+
+    @Column(name="website")
+    private String website;
+
+    @Column(name="openning_time")
+    private int openTime;
+
+    @Column(name = "closing_time")
+    private int closeTime;
+
+    @Column(name = "typical_price")
+    private double typicalPrice;
+
+    @Column(name = "additional_information")
+    private String additionalInformation;
+}
---
+++ backend/backendserver/src/main/java/entity/TicketImage.java
@@ -0,0 +1,26 @@
+package entity;
+
+import lombok.Data;
+
+import javax.persistence.*;
+
+@Data
+@Entity
+@Table(name="ticket_image")
+public class TicketImage {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name="image_id",columnDefinition = "INT(1)")
+    private int imageId;
+
+    @ManyToOne
+    @JoinColumn(name = "ticket_id", nullable = false)
+    private Ticket ticket;
+
+    @Column(name="url")
+    private String URL;
+
+    @Column(name="description")
+    private String description;
+
+}
