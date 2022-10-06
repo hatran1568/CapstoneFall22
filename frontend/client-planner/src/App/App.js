77434530d731bd8351b7db0013d023d5b5f3f4f8
Tran Thi Nguyet Ha <hattnhe153222@fpt.edu.vn>
2022-10-06 22:56:48 +0700

    Fix: client folder structure

---
+++ frontend/client-planner/src/App/App.js
@@ -0,0 +1,9 @@
+import "./App.css";
+import React from "react";
+import Routes from "../router/routes";
+
+function App() {
+  return <Routes></Routes>;
+}
+
+export default App;
---
+++ frontend/client-planner/src/views/Login/LoginRoutes.js
@@ -0,0 +1,13 @@
+import React from "react";
+import { Routes, Route } from "react-router-dom";
+import Signup from "./Signup";
+import Login from "./Login";
+
+export default function LoginRoutes() {
+  return (
+    <Routes>
+      <Route exact path="/login" element={<Login />} />
+      <Route exact path="/signup" element={<Signup />} />
+    </Routes>
+  );
+}
