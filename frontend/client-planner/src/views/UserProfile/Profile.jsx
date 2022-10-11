cd463ee0a1af9229fc8776ff038b8837f06febba
Tran Thi Nguyet Ha <hattnhe153222@fpt.edu.vn>
2022-10-11 15:05:40 +0700

    fix css import

--- frontend/client-planner/src/views/UserProfile/Profile.jsx
+++ frontend/client-planner/src/views/UserProfile/Profile.jsx
@@ -1,24 +1,27 @@
+import { MDBCard } from "mdb-react-ui-kit";
 import React from "react";
-import "./Profile.css";
+import style from "./Profile.module.css";
 function Profile() {
   return (
-    <div className="card p-3">
-      <div className="image d-flex flex-column justify-content-center align-items-center">
-        <button className="btn">
+    <MDBCard className={`${style.card} p-3`}>
+      <div
+        className={`${style.image} d-flex flex-column justify-content-center align-items-center`}
+      >
+        <button className={style.btn}>
           <img src="https://i.imgur.com/wvxPV9S.png" height="100" width="100" />
         </button>
-        <span className="name mt-3 mx-auto">Hatran</span>
-        <span className="email">test@test</span>
+        <span className={`${style.name} mt-3 mx-auto`}>Hatran</span>
+        <span className={style.email}>test@test</span>
 
-        <div className=" d-flex mt-2">
-          <button className="btn1 btn-dark">Edit Profile</button>
+        <div className="d-flex mt-2">
+          <button className={`${style.btn1} btn-dark`}>Edit Profile</button>
         </div>
 
-        <div className=" px-2 rounded mt-4 date ">
-          <span className="join">Joined May,2021</span>
+        <div className={`px-2 rounded mt-4 ${style.date}`}>
+          <span className={style.join}>Joined May,2021</span>
         </div>
       </div>
-    </div>
+    </MDBCard>
   );
 }
 
