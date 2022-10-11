f337299d0a9b779df605c31ac0bca052a90a14a4
DanielChendyrov <linhhonbaothu45@gmail.com>
2022-10-11 14:36:53 +0700

    fixing folder name

---
+++ backend/backendserver/src/main/java/com/planner/backendserver/controller/SearchPOIController.java
@@ -0,0 +1,5 @@
+package com.planner.backendserver.controller;
+
+public class SearchPOIController {
+    
+}
--- frontend/client-planner/src/views/HomePage/HomePage.jsx
+++ frontend/client-planner/src/views/HomePage/HomePage.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import "./HomePage.css";
-import POISearchBar from "../../components/POISearchBar/POISearchBar";
+import POISearchBar from "../../tempComp/POISearchBar/POISearchBar";
 
 function HomePage() {
     return (
