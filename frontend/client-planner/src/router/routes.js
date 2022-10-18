39f5bc885d80169333480dab4c919fa4265f5935
Ngoc Anh <ngocanh.a167@gmail.com>
2022-10-18 13:50:31 +0700

    fix: conflict

--- frontend/client-planner/src/router/routes.js
+++ frontend/client-planner/src/router/routes.js
@@ -2,21 +2,15 @@ import React from "react";
 import { BrowserRouter } from "react-router-dom";
 import LoginRoutes from "../views/Login/LoginRoutes";
 import ProfileRoute from "../views/UserProfile/ProfileRoute";
-<<<<<<< HEAD
 import TimelineRoute from "../views/Timeline/timelineRoute";
-=======
 import SearchRoutes from "../components/searchBar/SearchRoutes"
->>>>>>> 26ae37fc82c011b6c10ec552d0f2a3e08641bb46
 export default function RootRoutes() {
   return (
     <BrowserRouter>
       <LoginRoutes />
       <ProfileRoute />
-<<<<<<< HEAD
       <TimelineRoute/>
-=======
       <SearchRoutes/>
->>>>>>> 26ae37fc82c011b6c10ec552d0f2a3e08641bb46
     </BrowserRouter>
   );
 }
