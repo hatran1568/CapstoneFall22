d614bd3f6200bb6c748e895676564f607d4cd3af
Ngoc Anh <ngocanh.a167@gmail.com>
2022-10-26 23:22:38 +0700

    fix: dtos

--- backend/backendserver/src/main/java/com/planner/backendserver/repository/TripDetailRepository.java
+++ backend/backendserver/src/main/java/com/planner/backendserver/repository/TripDetailRepository.java
@@ -4,6 +4,7 @@ import com.planner.backendserver.entity.TripDetails;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 
+import java.util.ArrayList;
 import java.util.Optional;
 
 public interface TripDetailRepository extends JpaRepository<TripDetails, Integer> {
@@ -16,4 +17,10 @@ public interface TripDetailRepository extends JpaRepository<TripDetails, Integer
     Optional<TripDetails> getTripDetailsById(int id);
     @Override
     Optional<TripDetails> findById(Integer id);
+    @Query(value="select * from trip_details td where td.trip_id = :tripId order by td.trip_details_id asc limit 1",nativeQuery=true)
+    TripDetails findFirstInTrip(int tripId);
+
+    @Query("select td from TripDetails td where td.trip.tripId = :tripId")
+    ArrayList<TripDetails> getListByTripId(int tripId);
+
 }
--- backend/backendserver/src/main/java/com/planner/backendserver/service/interfaces/TripService.java
+++ backend/backendserver/src/main/java/com/planner/backendserver/service/interfaces/TripService.java
@@ -1,5 +1,6 @@
 package com.planner.backendserver.service.interfaces;
 
+import com.planner.backendserver.DTO.response.TripDetailedDTO;
 import com.planner.backendserver.dto.response.TripGeneralDTO;
 import com.planner.backendserver.entity.Trip;
 import com.planner.backendserver.entity.TripDetails;
@@ -10,7 +11,8 @@ import java.util.List;
 import java.util.Optional;
 @Service
 public interface TripService {
-    public Optional<Trip> getTripById(int id);
+    public TripDetailedDTO getTripDetailedById(int id);
+    public TripGeneralDTO getTripGeneralById(int id);
     public TripDetails addTripDetail(Date date, int startTime, int endTime, int activityId, int tripId);
     public TripDetails addCustomTripDetail(Date date, int startTime, int endTime, int tripId, String name, String address);
     public void deleteDetailById(int id);
