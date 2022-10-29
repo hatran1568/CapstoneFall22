92cffab193bf453ebf943d61ad2c6623b1e336be
Tran Thi Nguyet Ha <hattnhe153222@fpt.edu.vn>
2022-10-29 15:23:55 +0700

    Fix icons

--- frontend/client-planner/src/components/Trips/AddExpenseModal.jsx
+++ frontend/client-planner/src/components/Trips/AddExpenseModal.jsx
@@ -1,11 +1,25 @@
-import React from 'react'
-import {useState} from 'react';
+import React from "react";
+import { useState } from "react";
 import { useEffect } from "react";
 import axios from "axios";
-import Select from "react-dropdown-select"
+import Select from "react-dropdown-select";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import fontawesome from '@fortawesome/fontawesome'
-import { faFilter, faTrash, faPlane, faBed, faTaxi, faBus, faUtensils, faWineGlass, faMonument, faTicket, faBagShopping, faGasPump, faBasketShopping, faNoteSticky, faDongSign } from "@fortawesome/free-solid-svg-icons";
+import {
+  faFilter,
+  faPlane,
+  faBed,
+  faTaxi,
+  faBus,
+  faUtensils,
+  faWineGlass,
+  faMonument,
+  faTicket,
+  faBagShopping,
+  faGasPump,
+  faBasketShopping,
+  faNoteSticky,
+  faDongSign,
+} from "@fortawesome/free-solid-svg-icons";
 import style from "./AddExpenseModal.module.css";
 import {
   MDBBtn,
@@ -15,14 +29,9 @@ import {
   MDBModalHeader,
   MDBModalTitle,
   MDBModalBody,
-  MDBBtnGroup,
-  MDBRadio,
-  MDBModalFooter,
-  MDBInput,
   MDBRow,
-  MDBCol
+  MDBCol,
 } from "mdb-react-ui-kit";
-fontawesome.library.add(faFilter, faTrash, faPlane, faBed, faTaxi, faBus, faUtensils, faWineGlass, faMonument, faTicket, faBagShopping, faGasPump, faBasketShopping, faNoteSticky, faDongSign);
 function AddExpenseModal() {
   //Get category Data
   var currentCat = 0;
@@ -31,33 +40,33 @@ function AddExpenseModal() {
     document.getElementById("modalCat").value = event.target.name;
   };
   //Add Expense
-  const addExpense = event => {
-    const id = window.location.href.split('/')[4];
+  const addExpense = (event) => {
+    const id = window.location.href.split("/")[4];
     if (document.getElementById("modalDesc").value == null)
-      document.getElementById("modalDesc").value = " "
-    if (document.getElementById("modalAmount").value=="")
-      {
-        document.getElementById("errorMessage").innerHTML = "Please enter an amount.";
-      }
-    else if (currentCat == 0)
-    document.getElementById("errorMessage").innerHTML = "Please select a category.";
+      document.getElementById("modalDesc").value = " ";
+    if (document.getElementById("modalAmount").value == "") {
+      document.getElementById("errorMessage").innerHTML =
+        "Please enter an amount.";
+    } else if (currentCat == 0)
+      document.getElementById("errorMessage").innerHTML =
+        "Please select a category.";
     else
-    axios({
-        method: 'post',
-        url: 'http://localhost:8080/api/expense/new',
+      axios({
+        method: "post",
+        url: "http://localhost:8080/api/expense/new",
         data: {
-            amount: document.getElementById("modalAmount").value,
-            description: document.getElementById("modalDesc").value,
-            expenseCategoryId: currentCat,
-            tripId: id,
-            expenseId: 0
+          amount: document.getElementById("modalAmount").value,
+          description: document.getElementById("modalDesc").value,
+          expenseCategoryId: currentCat,
+          tripId: id,
+          expenseId: 0,
         },
         headers: {
-            'Content-Type': 'application/json'
-        }
+          "Content-Type": "application/json",
+        },
       }).then(function (response) {
         window.location.reload();
-    });
+      });
   };
   //Add Expense Modal
   const [basicModal, setBasicModal] = useState(false);
@@ -71,124 +80,235 @@ function AddExpenseModal() {
   };
   return (
     <span>
-      <MDBBtn color='info' onClick={toggleShow}>
+      <MDBBtn color="info" onClick={toggleShow}>
         Add Expense
       </MDBBtn>
-      <MDBModal show={basicModal} setShow={setBasicModal} tabIndex='-1'>
+      <MDBModal show={basicModal} setShow={setBasicModal} tabIndex="-1">
         <MDBModalDialog>
           <MDBModalContent className={style.modalContainer}>
             <MDBModalHeader>
               <MDBModalTitle>Add Expense</MDBModalTitle>
-              <MDBBtn className='btn-close' color='none' onClick={toggleShow}></MDBBtn>
+              <MDBBtn
+                className="btn-close"
+                color="none"
+                onClick={toggleShow}
+              ></MDBBtn>
             </MDBModalHeader>
             <MDBModalBody className={style.modalBody}>
               <div className="input-group mb-3">
-                <span className="input-group-text"><FontAwesomeIcon icon="dong-sign"/></span>
+                <span className="input-group-text">
+                  <FontAwesomeIcon icon={faDongSign} />
+                </span>
                 <input
                   type="number"
                   className="form-control"
                   id="modalAmount"
-                  label='Amount'
-                  placeholder='Amount'
+                  label="Amount"
+                  placeholder="Amount"
                 />
               </div>
               <div className="input-group mb-3">
-                <span className="input-group-text"><FontAwesomeIcon icon="note-sticky"/></span>
+                <span className="input-group-text">
+                  <FontAwesomeIcon icon={faNoteSticky} />
+                </span>
                 <input
                   type="text"
                   className="form-control"
                   id="modalDesc"
-                  label='Description'
-                  placeholder='Description'
+                  label="Description"
+                  placeholder="Description"
                 />
               </div>
               <div className="input-group mb-3">
-                <span className="input-group-text"><FontAwesomeIcon icon="filter"/></span>
+                <span className="input-group-text">
+                  <FontAwesomeIcon icon={faFilter} />
+                </span>
                 <input
                   type="text"
                   className="form-control"
                   id="modalCat"
-                  label='Category'
-                  placeholder='Category'
+                  label="Category"
+                  placeholder="Category"
                   disabled
                 />
               </div>
               <MDBRow>
                 <MDBCol>
-                  <MDBBtn color="dark" id={1} onClick={changeCat} name="Flight" className={style.catBtn}>
-                    <FontAwesomeIcon icon="plane"/><br/>Flight
+                  <MDBBtn
+                    color="dark"
+                    id={1}
+                    onClick={changeCat}
+                    name="Flight"
+                    className={style.catBtn}
+                  >
+                    <FontAwesomeIcon icon={faPlane} />
+                    <br />
+                    Flight
                   </MDBBtn>
                 </MDBCol>
                 <MDBCol>
-                  <MDBBtn color="dark" id={2} onClick={changeCat} name="Lodging" className={style.catBtn}>
-                    <FontAwesomeIcon icon="bed"/><br/>Lodging
+                  <MDBBtn
+                    color="dark"
+                    id={2}
+                    onClick={changeCat}
+                    name="Lodging"
+                    className={style.catBtn}
+                  >
+                    <FontAwesomeIcon icon={faBed} />
+                    <br />
+                    Lodging
                   </MDBBtn>
                 </MDBCol>
                 <MDBCol>
-                  <MDBBtn color="dark" id={3} onClick={changeCat} name="Car Rental" className={style.catBtn}>
-                    <FontAwesomeIcon icon="taxi"/><br/>Car Rental
+                  <MDBBtn
+                    color="dark"
+                    id={3}
+                    onClick={changeCat}
+                    name="Car Rental"
+                    className={style.catBtn}
+                  >
+                    <FontAwesomeIcon icon={faTaxi} />
+                    <br />
+                    Car Rental
                   </MDBBtn>
                 </MDBCol>
                 <MDBCol>
-                  <MDBBtn color="dark" id={4} onClick={changeCat} name="Public Transit" className={style.catBtn}>
-                    <FontAwesomeIcon icon="bus"/><br/>Public Transit
+                  <MDBBtn
+                    color="dark"
+                    id={4}
+                    onClick={changeCat}
+                    name="Public Transit"
+                    className={style.catBtn}
+                  >
+                    <FontAwesomeIcon icon={faBus} />
+                    <br />
+                    Public Transit
                   </MDBBtn>
                 </MDBCol>
-              </MDBRow><br/>
+              </MDBRow>
+              <br />
               <MDBRow>
                 <MDBCol>
-                  <MDBBtn color="dark" id={5} onClick={changeCat} name="Food" className={style.catBtn}>
-                    <FontAwesomeIcon icon="utensils"/><br/>Food
+                  <MDBBtn
+                    color="dark"
+                    id={5}
+                    onClick={changeCat}
+                    name="Food"
+                    className={style.catBtn}
+                  >
+                    <FontAwesomeIcon icon={faUtensils} />
+                    <br />
+                    Food
                   </MDBBtn>
                 </MDBCol>
                 <MDBCol>
-                  <MDBBtn color="dark" id={6} onClick={changeCat} name="Drinks" className={style.catBtn}>
-                    <FontAwesomeIcon icon="wine-glass"/><br/>Drinks
+                  <MDBBtn
+                    color="dark"
+                    id={6}
+                    onClick={changeCat}
+                    name="Drinks"
+                    className={style.catBtn}
+                  >
+                    <FontAwesomeIcon icon={faWineGlass} />
+                    <br />
+                    Drinks
                   </MDBBtn>
                 </MDBCol>
                 <MDBCol>
-                  <MDBBtn color="dark" id={7} onClick={changeCat} name="Sightseeing" className={style.catBtn}>
-                    <FontAwesomeIcon icon="monument"/><br/>Sightseeing
+                  <MDBBtn
+                    color="dark"
+                    id={7}
+                    onClick={changeCat}
+                    name="Sightseeing"
+                    className={style.catBtn}
+                  >
+                    <FontAwesomeIcon icon={faMonument} />
+                    <br />
+                    Sightseeing
                   </MDBBtn>
                 </MDBCol>
                 <MDBCol>
-                  <MDBBtn color="dark" id={8} onClick={changeCat} name="Activities" className={style.catBtn}>
-                    <FontAwesomeIcon icon="ticket"/><br/>Activities
+                  <MDBBtn
+                    color="dark"
+                    id={8}
+                    onClick={changeCat}
+                    name="Activities"
+                    className={style.catBtn}
+                  >
+                    <FontAwesomeIcon icon={faTicket} />
+                    <br />
+                    Activities
                   </MDBBtn>
                 </MDBCol>
-              </MDBRow><br/>
+              </MDBRow>
+              <br />
               <MDBRow>
                 <MDBCol>
-                  <MDBBtn color="dark" id={9} onClick={changeCat} name="Shopping" className={style.catBtn}>
-                    <FontAwesomeIcon icon="bag-shopping"/><br/>Shopping
+                  <MDBBtn
+                    color="dark"
+                    id={9}
+                    onClick={changeCat}
+                    name="Shopping"
+                    className={style.catBtn}
+                  >
+                    <FontAwesomeIcon icon={faBagShopping} />
+                    <br />
+                    Shopping
                   </MDBBtn>
                 </MDBCol>
                 <MDBCol>
-                  <MDBBtn color="dark" id={10} onClick={changeCat} name="Gas" className={style.catBtn}>
-                    <FontAwesomeIcon icon="gas-pump"/><br/>Gas
+                  <MDBBtn
+                    color="dark"
+                    id={10}
+                    onClick={changeCat}
+                    name="Gas"
+                    className={style.catBtn}
+                  >
+                    <FontAwesomeIcon icon={faGasPump} />
+                    <br />
+                    Gas
                   </MDBBtn>
                 </MDBCol>
                 <MDBCol>
-                  <MDBBtn color="dark" id={11} onClick={changeCat} name="Groceries" className={style.catBtn}>
-                    <FontAwesomeIcon icon="basket-shopping"/><br/>Groceries
+                  <MDBBtn
+                    color="dark"
+                    id={11}
+                    onClick={changeCat}
+                    name="Groceries"
+                    className={style.catBtn}
+                  >
+                    <FontAwesomeIcon icon={faBasketShopping} />
+                    <br />
+                    Groceries
                   </MDBBtn>
                 </MDBCol>
                 <MDBCol>
-                  <MDBBtn color="dark" id={12} onClick={changeCat} name="Other" className={style.catBtn}>
-                    <FontAwesomeIcon icon="note-sticky"/><br/>Other
+                  <MDBBtn
+                    color="dark"
+                    id={12}
+                    onClick={changeCat}
+                    name="Other"
+                    className={style.catBtn}
+                  >
+                    <FontAwesomeIcon icon={faNoteSticky} />
+                    <br />
+                    Other
                   </MDBBtn>
                 </MDBCol>
-              </MDBRow><br/>
+              </MDBRow>
+              <br />
               <div id="errorMessage" className={style.errorMessage}></div>
-              <br/>
+              <br />
               <div className={style.modalBtn}>
-                <MDBBtn color="info" onClick={addExpense}>Save</MDBBtn>
+                <MDBBtn color="info" onClick={addExpense}>
+                  Save
+                </MDBBtn>
               </div>
             </MDBModalBody>
           </MDBModalContent>
         </MDBModalDialog>
       </MDBModal>
     </span>
-  )
+  );
 }
-export default AddExpenseModal
+export default AddExpenseModal;
--- frontend/client-planner/src/views/Blog/BlogDetails.jsx
+++ frontend/client-planner/src/views/Blog/BlogDetails.jsx
@@ -4,8 +4,12 @@ import { useState } from "react";
 import { useEffect } from "react";
 import { useParams } from "react-router-dom";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import fontawesome from '@fortawesome/fontawesome'
-import { faArrowRight, faClock, faArrowLeft, faUser } from "@fortawesome/free-solid-svg-icons";
+import {
+  faArrowRight,
+  faClock,
+  faArrowLeft,
+  faUser,
+} from "@fortawesome/free-solid-svg-icons";
 import axios from "../../api/axios";
 import {
   MDBBtn,
@@ -16,7 +20,6 @@ import {
   MDBCol,
 } from "mdb-react-ui-kit";
 import style from "./BlogDetails.module.css";
-fontawesome.library.add( faArrowRight, faClock, faArrowLeft, faUser );
 
 function BlogDetails() {
   const [isLoading, setLoading] = useState(true);
@@ -47,10 +50,14 @@ function BlogDetails() {
     return (
       <MDBContainer className={style.errorContainer}>
         <h1>This blog is not yet Published or has been Hidden or Deleted</h1>
-        <b><a href="" className={style.returnBlogBtn}>Return to Blog list</a></b>
+        <b>
+          <a href="" className={style.returnBlogBtn}>
+            Return to Blog list
+          </a>
+        </b>
       </MDBContainer>
-    )
-  else if (!isLoading){
+    );
+  else if (!isLoading) {
     document.title = blog.title + " | Tripplanner";
     const dateRaw = blog.dateModified;
     var options = {
@@ -63,50 +70,77 @@ function BlogDetails() {
     console.log(date);
     return (
       <div>
-        <img src={blog.thumbnail} className={style.thumbnail}/>
+        <img src={blog.thumbnail} className={style.thumbnail} />
         <MDBContainer className={style.mainContainer}>
           <MDBCard className={style.mainCard}>
             <MDBCardBody className={style.mainCardBody}>
-              <h1>{blog.title}</h1><br/>
+              <h1>{blog.title}</h1>
+              <br />
               <div className={style.dateBox}>
-                <FontAwesomeIcon icon="clock" size="sm"/>
+                <FontAwesomeIcon icon={faClock} size="sm" />
                 <span className={style.date}>{date}</span>
-                <FontAwesomeIcon icon="user" size="sm"/>
+                <FontAwesomeIcon icon={faUser} size="sm" />
                 <span className={style.date}>{blog.username}</span>
               </div>
-              <div dangerouslySetInnerHTML={{__html:blog.content}} className={style.contentContainer}>            
-              </div>
+              <div
+                dangerouslySetInnerHTML={{ __html: blog.content }}
+                className={style.contentContainer}
+              ></div>
               <MDBRow className={style.authorBox}>
                 <MDBCol md={1}>
-                  <img src={blog.avatar} className={style.avatar}/>
+                  <img src={blog.avatar} className={style.avatar} />
                 </MDBCol>
                 <MDBCol md={8}>
-                  <b>Author</b><br/>
+                  <b>Author</b>
+                  <br />
                   {blog.username}
                 </MDBCol>
               </MDBRow>
             </MDBCardBody>
-
           </MDBCard>
           <MDBCard id="navContainer" className={style.navContainer}>
             <MDBRow>
               <MDBCol className={style.arrowIconsBox}>
-                <a href={"blog?id=" + nearbyBlogs[0].blogId} className={style.nearbyBlogBtn}>
-                <FontAwesomeIcon className={style.arrowIcons} icon="arrow-left"/></a>
+                <a
+                  href={"blog?id=" + nearbyBlogs[0].blogId}
+                  className={style.nearbyBlogBtn}
+                >
+                  <FontAwesomeIcon
+                    className={style.arrowIcons}
+                    icon={faArrowLeft}
+                  />
+                </a>
               </MDBCol>
               <MDBCol className={style.lastBlogBox}>
-                <a href={"blog?id=" + nearbyBlogs[0].blogId} className={style.nearbyBlogBtn}>
-                  <div className={style.nearbyTitle}>{nearbyBlogs[0].title}</div>
+                <a
+                  href={"blog?id=" + nearbyBlogs[0].blogId}
+                  className={style.nearbyBlogBtn}
+                >
+                  <div className={style.nearbyTitle}>
+                    {nearbyBlogs[0].title}
+                  </div>
                 </a>
               </MDBCol>
               <MDBCol className={style.nextBlogBox}>
-                <a href={"blog?id=" + nearbyBlogs[1].blogId} className={style.nearbyBlogBtn}>
-                  <div className={style.nearbyTitle}>{nearbyBlogs[1].title}</div>
+                <a
+                  href={"blog?id=" + nearbyBlogs[1].blogId}
+                  className={style.nearbyBlogBtn}
+                >
+                  <div className={style.nearbyTitle}>
+                    {nearbyBlogs[1].title}
+                  </div>
                 </a>
               </MDBCol>
               <MDBCol className={style.arrowIconsBox}>
-                <a href={"blog?id=" + nearbyBlogs[1].blogId} className={style.nearbyBlogBtn}>
-                <FontAwesomeIcon className={style.arrowIcons} icon="arrow-right"/></a>
+                <a
+                  href={"blog?id=" + nearbyBlogs[1].blogId}
+                  className={style.nearbyBlogBtn}
+                >
+                  <FontAwesomeIcon
+                    className={style.arrowIcons}
+                    icon={faArrowRight}
+                  />
+                </a>
               </MDBCol>
             </MDBRow>
           </MDBCard>
@@ -114,6 +148,5 @@ function BlogDetails() {
       </div>
     );
   }
-    
 }
 export default BlogDetails;
