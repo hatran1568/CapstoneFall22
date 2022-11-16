38ffdd6e5b0a7ff93860bc908ab3ae5255944275
DanielChendyrov <linhhonbaothu45@gmail.com>
2022-11-16 14:37:13 +0700

    test

--- backend/backendserver/src/test/java/com/planner/backendserver/controller/LoginControllerTest.java
+++ backend/backendserver/src/test/java/com/planner/backendserver/controller/LoginControllerTest.java
@@ -1,18 +1,43 @@
 package com.planner.backendserver.controller;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.planner.backendserver.config.JwtTokenProvider;
+import com.planner.backendserver.repository.UserRepository;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.test.web.servlet.MockMvc;
 
+import static org.junit.jupiter.api.Assertions.*;
+
+@WebMvcTest(controllers = LoginController.class)
 class LoginControllerTest {
-    LoginController loginController;
+    @MockBean
+    private UserRepository userRepository;
+
+    @MockBean
+    private AuthenticationManager authenticationManager;
+
+    @MockBean
+    private JwtTokenProvider tokenProvider;
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Autowired
+    private ObjectMapper mapper;
 
     @BeforeEach
-    public void setUp() {
-        loginController = new LoginController();
+    void setUp() {
     }
 
     @Test
-    public void shouldAuthenticateUser(){
-
+    void authenticateUserTest() {
+        Mockito.when(authenticationManager.authenticate(Mockito.any(UsernamePasswordAuthenticationToken.class)));
     }
 }
